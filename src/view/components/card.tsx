981e4113791301ee5083411b9e428899389ea453
bigben3918 <benjamin199551@gmail.com>
2022-12-05 08:49:41 +0800

    update: links

--- src/view/components/card.tsx
+++ src/view/components/card.tsx
@@ -115,6 +115,7 @@ function Card(props: BooksObject) {
                                         href={infoLink}
                                         className="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1"
                                         target={"_blank"}
+                                        rel="noopener noreferrer"
                                     >
                                         Link
                                     </a>
